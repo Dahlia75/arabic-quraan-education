16c298753b375156d1a89ba7c1c779f5ef9102b6
Dahlia <d.aljabari75@gmail.com>
2020-03-31 23:25:39 -0700

    connect with espress

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = express();
 const port = 3000;
 
 //Middlewares
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
